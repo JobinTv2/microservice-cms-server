12a872b7930cf8bd2fc4542478e8f42009e5fa70
Jobin T v <jobintv@focaloid.com>
2022-12-12 13:48:12 +0530

    feat: Role based authorization done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,19 @@
 import { Post, Body, Controller, Get, UseGuards, Param } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/auth-guard/jwt-auth-guard';
 import { LocalAuthGuard } from 'src/auth/auth-guard/local-auth-guard';
+import { RolesGaurd } from 'src/auth/auth-guard/roles-gaurd';
+import { HasRoles } from 'src/auth/has-roles.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { Role } from './dto/user-role.dot';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @HasRoles(Role.User)
+  @UseGuards(JwtAuthGuard, RolesGaurd)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
